32db95a886cd97179b6c6c7cb690fcd83bbe99be
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-05-24 17:47:10 -0400

    outputting list data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -18,9 +18,19 @@ const PostList = ({ isPosting, onStopPosting }) => {
 					<NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
 				</Modal>
 			)}
-			<ul className={classes.posts}>
-				<Post author="Lena" body="I love React.js!" />
-			</ul>
+			{posts.length > 0 && (
+				<ul className={classes.posts}>
+					{posts.map((post) => (
+						<Post key={post.body} author={post.author} body={post.body} />
+					))}
+				</ul>
+			)}
+			{posts.length === 0 && (
+				<div style={{ textAlign: "center", color: "white" }}>
+					<h2>No posts at the moment!</h2>
+					<p>Start adding some!</p>
+				</div>
+			)}
 		</>
 	);
 };
